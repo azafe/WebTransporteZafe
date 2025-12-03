cfe9ab11890c3fe92f3fdcefc5140e0181eb9ea1
Alvaro Zafe <alvarozafe@gmail.com>
2025-12-02 21:36:21 -0300

    Nuevo orden

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,29 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
-import About from './components/About';
-import Fleet from './components/Fleet';
-import Services from './components/Services';
+import Services from './components/Services'; // Subimos Servicios
+import Fleet from './components/Fleet';       // Subimos Flota
+import Clients from './components/Clients';   // Tu nuevo "CV"
+import About from './components/About';       // Bajamos la historia al final
 import Footer from './components/Footer';
-import WhatsAppBtn from './components/WhatsAppBtn'; 
-import Clients from './components/Clients';
-// Importamos el botón
+import WhatsAppBtn from './components/WhatsAppBtn';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Hero />
-      <About />
+      
+      {/* BLOQUE DE VENTA DIRECTA */}
+      <Services /> 
       <Fleet />
-      <Services />
+      
+      {/* BLOQUE DE CONFIANZA */}
       <Clients />
+      <About />
+      
       <Footer />
-      <WhatsAppBtn /> {/* Lo añadimos aquí para que flote sobre todo */}
+      <WhatsAppBtn />
     </div>
   );
 }
